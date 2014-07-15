3fe642805c8c216b7b8a234d6b614f2bdb5f66c3
Vadzim_Bura <vadson93@gmail.com>
2014-07-15 11:52:47 +0300

    Add setActiveTab and changeActiveTab methods.

--- scripts/jquery.tabControl.js
+++ scripts/jquery.tabControl.js
@@ -1,27 +1,53 @@
 (function($){
     $.fn.tabControl = function(options){
-        var settings = $.extend({}, $.fn.tabControl, $.fn.tabControl.defaults);
+        var $elem = this;
+        var settings = $.extend({}, $.fn.tabControl.defaults, options);
+        var tabHeaders;
+        var tabHeadersItems; //= tabHeaders.find('li');
+        var tabContainers; //= $('.tab-content', $elem);
 
-        
+        function setActiveTab(){
+            var selectedTab = settings.selected < tabHeadersItems.length && settings.selected >= 0 ?
+                              settings.selected : $.fn.tabControl.defaults.selected;
+            changeActiveTab(tabHeaders.children().eq(selectedTab));
+        }
+
+        function changeActiveTab(newActiveTab){
+            var targetIndex = tabHeaders.children().index(newActiveTab);
+
+            if(targetIndex == -1){
+                return;
+            }
+            tabHeadersItems.removeClass('active');
+            $(newActiveTab).addClass('active');
+
+            tabContainers.removeClass('active');
+            $(tabContainers[targetIndex]).addClass('active');
+        }
+
+        function generateMarkup(){
+
+
+        }
 
         return this.each(function(){
+            if(settings.init === true){
+                generateMarkup();
 
-            var tabHeaders = $('.tab-menu');
-            var tabHeadersItems = tabHeaders.find('li');
-            var tabContainers = $('.tab-content');
+                tabHeaders = $('.tab-menu', $elem);
+                tabHeadersItems = tabHeaders.find('li');
+                tabContainers = $('.tab-content', $elem);
+            }
+            setActiveTab();
+
             tabHeaders.click(function(e){
-                var targetIndex = tabHeaders.children().index(e.target);
-
-                tabHeadersItems.removeClass('active');
-                $(e.target).addClass('active');
-
-                tabContainers.removeClass('active');
-                $(tabContainers[targetIndex]).addClass('active');
+                changeActiveTab(e.target);
             });
         });
     };
 
     $.fn.tabControl.defaults = {
-        selected: 0
+        selected: 0,
+        init: false
     }
 })(jQuery);
